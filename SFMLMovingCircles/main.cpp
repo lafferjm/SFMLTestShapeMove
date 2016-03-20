3f99bf166e08a7fcef4340d30ae0df17c740bb26
Justin Lafferty <lafferjm@gmail.com>
2016-03-20 14:08:48 -0500

    Three circles are now drawn to the screen.

--- SFMLMovingCircles/main.cpp
+++ SFMLMovingCircles/main.cpp
@@ -6,8 +6,31 @@
 int main(int argc, char** argv)
 {
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Moving Circles");
 
+    //Create three circles with a 50 pixel radius
+    sf::CircleShape circleRed(50);
+    sf::CircleShape circleGreen(50);
+    sf::CircleShape circleBlue(50);
+    
+    //Add the appropriate colors to our circles
+    circleRed.setFillColor(sf::Color(255, 0, 0));
+    circleGreen.setFillColor(sf::Color(0, 255, 0));
+    circleBlue.setFillColor(sf::Color(0, 0, 255));
+    
+    float xRed = 100;
+    float yRed = 100;
+    
+    float xGreen = 200;
+    float yGreen = 200;
+    
+    float xBlue = 300;
+    float yBlue = 300;
+    
+    circleRed.setPosition(xRed, yRed);
+    circleGreen.setPosition(xGreen, yGreen);
+    circleBlue.setPosition(xBlue, yBlue);
+    
     while (window.isOpen())
     {
 
@@ -15,16 +38,18 @@ int main(int argc, char** argv)
         while (window.pollEvent(event))
         {
 
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
 
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
         }
 
         window.clear();
+        
+        window.draw(circleRed);
+        window.draw(circleGreen);
+        window.draw(circleBlue);
 
         window.display();
     }
